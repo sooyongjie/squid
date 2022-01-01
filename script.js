47097b94ff1d6299c6b7a25ef964065b68dda280
Soo Yong Jie <sooyongjie@gmail.com>
2022-01-01 16:56:59 +0800

    Styling updates & added download info

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const apiKey = document.querySelector('#apiKey')
 const downloadLink = document.querySelector('#downloadLink')
 const submitBtn = document.querySelector('#submitBtn')
 
+const results = document.querySelector('.results')
+const host = document.querySelector('#host')
+const fileName = document.querySelector('#filename')
+const fileSize = document.querySelector('#filesize')
+
+let test = 0;
+
 let data = {
     "link": "https://r8j4ym.debrid.it/dl/testing123/Pok%C3%A9mon%20Mystery%20Dungeon%20Rescue%20Team%20DX%20%28NSP%29%28Base%20Game%29.rar",
     "host": "1fichier",
@@ -14,9 +21,7 @@ let data = {
     "hostDomain": "1fichier.com"
 }
 
-console.log(data);
-
-if(Cookies.get('key')) {
+if (Cookies.get('key')) {
     apiName.value = Cookies.get('name')
     apiKey.value = Cookies.get('key')
     downloadLink.value = Cookies.get('url')
@@ -31,24 +36,36 @@ const fetchLink = (name, key, url) => {
     Cookies.set('key', key, { expires: 30 })
     Cookies.set('url', url, { expires: 30 })
     let api = `https://api.alldebrid.com/v4/link/unlock?agent=${name}&apikey=${key}&link=${url}`
-    let test = 0;
+    console.log('api: ', api);
 
     if (!test) {
         axios.get(api)
-            .then(response => {
-                const data = response.data.data;
-                console.log(`data: `, data);
-                openLink(data.link)
+            .then(res => {
+                console.log('res: ', res);
+                const data = res.data.data;
+                console.log('data: ', data);
+                showResults(data)
             })
             .catch(error => console.error(error));
     } else {
-        openLink(data.link)
+        showResults(data)
     }
 };
 
+const showResults = (data) => {
+    setTimeout(() => {
+        results.style.opacity = 1;
+    }, 50);
+
+    host.textContent = data.host
+    filename.textContent = data.filename
+    fileSize.textContent = filesize(data.filesize)
+    // openLink(data.link)
+}
+
 const openLink = (resUrl) => {
     let openLink = confirm("Open link?");
-    if(openLink != null) {
+    if (openLink) {
         window.location.href = resUrl
     }
 }
